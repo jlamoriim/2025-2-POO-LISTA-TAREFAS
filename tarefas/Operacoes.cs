6ee49a36fabc236875ae8fbe04afc569b3476f3b
Julia Amorim <julial.brito@uni9.edu.br>
2025-10-07 00:14:55 +0000

    feat: classe tarefa

---
+++ tarefas/Operacoes.cs
@@ -0,0 +1,27 @@
+public class Operacoes
+{
+    public int Criar(Tarefa tarefa)
+    {
+        return 0;
+    }
+
+    public Tarefas Buscar (int id)
+    {
+        return null;
+    }
+
+    public List<Tarefa> Liatar()
+    {
+        return Array.Empty<Tarefa>();
+    }
+
+    public void Alterar(Tarefa tarefa)
+    {
+        
+    }
+
+    public void Excluir (int id)
+    {
+
+    }
+}
---
+++ tarefas/Tarefas.cs
@@ -0,0 +1,11 @@
+public class Tarefa
+{
+    
+public id int {get; set;}
+public nome string {get; set;}
+public descricao string {get; set;}
+public dataCriacao DateTime {get; set;}
+public status int {get; set;}
+public dataExecucao DateTime {get; set;}
+
+}
